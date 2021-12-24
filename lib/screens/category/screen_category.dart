6b7bdb134fe5ecf265dd73929944d5a2db8f0bd6
Mohammed Mirsa <mirsa@Mirsa.Mirsa>
2021-12-24 15:36:22 +0530

    Tab bar is set in category page. 2 Tabbar named as Income and Expense

--- lib/screens/category/screen_category.dart
+++ lib/screens/category/screen_category.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ScreenCategory extends StatelessWidget {
+class ScreenCategory extends StatefulWidget {
   const ScreenCategory({Key? key}) : super(key: key);
 
   @override
+  State<ScreenCategory> createState() => _ScreenCategoryState();
+}
+
+class _ScreenCategoryState extends State<ScreenCategory>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Category'));
+    return Column(
+      children: [
+        TabBar(controller: _tabController, 
+        labelColor: Colors.black,
+        unselectedLabelColor: Colors.grey,
+        tabs: [
+          Tab(
+            text: 'INCOME',
+          ),
+          Tab(
+            text: 'EXPENSE',
+          ),
+        ])
+      ],
+    );
   }
 }
