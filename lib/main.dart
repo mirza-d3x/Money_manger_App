b9c9974b180545cc52003fb335c68ab020fc6f77
Mohammed Mirsa <mirsa@Mirsa.Mirsa>
2021-12-26 15:54:27 +0530

    category model and category type adapter registering

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:money_manager/models/category/category_model.dart';
 import 'package:money_manager/screens/home/screen_home.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)){
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)){
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
   runApp(const MyApp());
 }
 
--- lib/models/category/category_model.dart
+++ lib/models/category/category_model.dart
@@ -1,7 +1,12 @@
 import 'package:hive_flutter/adapters.dart';
+part 'category_model.g.dart';
 
+@HiveType(typeId: 2)
 enum CategoryType {
+  @HiveField(0)
   income,
+
+  @HiveField(1)
   expense,
 }
 
