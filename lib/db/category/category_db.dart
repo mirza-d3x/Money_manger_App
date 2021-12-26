9b6cd722b8928a8d0d664ce2743c1ef6fb6ff601
Mohammed Mirsa <mirsa@Mirsa.Mirsa>
2021-12-26 16:22:50 +0530

    Hive Database opening openBox

--- lib/db/category/category_db.dart
+++ lib/db/category/category_db.dart
@@ -1,3 +1,4 @@
+import 'package:hive/hive.dart';
 import 'package:money_manager/models/category/category_model.dart';
 
 abstract class CategoryDbFunctions {
@@ -5,8 +6,12 @@ abstract class CategoryDbFunctions {
   Future<void> insertCategory(CategoryModel value);
 }
 
+const CATEGORY_DB_NAME = 'category-database';
+
 class CategoryDb implements CategoryDbFunctions {
   @override
-  Future<void> insertCategory(CategoryModel value) async {}
-  
+  Future<void> insertCategory(CategoryModel value) async {
+    final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    await _categoryDB.add(value);
+  }
 }
