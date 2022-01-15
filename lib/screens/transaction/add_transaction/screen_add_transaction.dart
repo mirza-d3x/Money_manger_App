073cca0dd47661a79451375346b121831224a8b7
Mohammed Mirsa <mirsa@Mirsa.Mirsa>
2022-01-15 12:59:55 +0530

    After a long break restarted my project

--- lib/screens/transaction/add_transaction/screen_add_transaction.dart
+++ lib/screens/transaction/add_transaction/screen_add_transaction.dart
@@ -2,12 +2,53 @@ import 'package:flutter/material.dart';
 
 class ScreenAddTransaction extends StatelessWidget {
   static const routeName = 'add_transaction';
-  const ScreenAddTransaction({ Key? key }) : super(key: key);
+  const ScreenAddTransaction({Key? key}) : super(key: key);
+
+  /*
+  Purpose of Transaction
+  Date of transaction
+  Amount
+  Category Type
+  Income/Expense radio Button
+  */
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Add Transaction'),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: 'Purpose',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Amount',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
