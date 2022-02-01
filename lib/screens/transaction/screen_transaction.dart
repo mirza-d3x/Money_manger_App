1834f4becd883e5d3f1c66b5f1d9e357e464458e
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-02-01 15:35:34 +0530

    income expense total display ui created

--- lib/screens/transaction/screen_transaction.dart
+++ lib/screens/transaction/screen_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:money_manager/db/category/category_db.dart';
 import 'package:money_manager/db/transaction/transaction_db.dart';
@@ -13,83 +14,140 @@ class ScreenTransaction extends StatelessWidget {
   Widget build(BuildContext context) {
     TransactionDB.instance.refresh();
     CategoryDB.instance.refreshUI();
-    return ValueListenableBuilder(
-      valueListenable: TransactionDB.instance.transactionListNotifier,
-      builder: (BuildContext ctx, List<TransactionModel> newList, Widget? _) {
-        return ListView.separated(
-          padding: const EdgeInsets.all(10),
-          itemBuilder: (ctx, index) {
-            final _value = newList[index];
-            // slidable function to see delete button when swipe transaction
-            return Slidable(
-              key: Key(_value.id!),
-              startActionPane: ActionPane(
-                motion: const ScrollMotion(),
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  SlidableAction(
-                    onPressed: (ctx) {
-                      TransactionDB.instance.deleteTransaction(_value.id!);
-                    },
-                    icon: Icons.delete,
-                    label: 'Delete',
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Income',
+                          style: GoogleFonts.rubik(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '99999',
+                          style: GoogleFonts.rubik(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Expense',
+                          style: GoogleFonts.rubik(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '99999',
+                          style: GoogleFonts.rubik(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-              child: Card(
-                elevation: 0,
-                child: ListTile(
-                  // Date
-                  leading: CircleAvatar(
-                    radius: 50,
-                    child: Text(
-                      parseDate(_value.date),
-                      textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: TransactionDB.instance.transactionListNotifier,
+        builder: (BuildContext ctx, List<TransactionModel> newList, Widget? _) {
+          return ListView.separated(
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (ctx, index) {
+              final _value = newList[index];
+              // slidable function to see delete button when swipe transaction
+              return Slidable(
+                key: Key(_value.id!),
+                startActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (ctx) {
+                        TransactionDB.instance.deleteTransaction(_value.id!);
+                      },
+                      icon: Icons.delete,
+                      label: 'Delete',
+                    ),
+                  ],
+                ),
+                child: Card(
+                  elevation: 0,
+                  child: ListTile(
+                    // Date
+                    leading: CircleAvatar(
+                      radius: 50,
+                      child: Text(
+                        parseDate(_value.date),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Raleway',
+                        ),
+                      ),
+                      // change color to find income and expense easily
+                      backgroundColor: _value.type == CategoryType.income
+                          ? Colors.green
+                          : Colors.red,
+                    ),
+                    // Amount
+                    title: Text(
+                      'RS ${_value.amount}',
                       style: const TextStyle(
-                        fontWeight: FontWeight.bold,
                         color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                         fontFamily: 'Raleway',
                       ),
                     ),
-                    // change color to find income and expense easily
-                    backgroundColor: _value.type == CategoryType.income
-                        ? Colors.green
-                        : Colors.red,
-                  ),
-                  // Amount
-                  title: Text(
-                    'RS ${_value.amount}',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      fontFamily: 'Raleway',
+                    // category
+                    subtitle: Text(
+                      _value.category.name,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          fontFamily: 'RaleWay'),
+                    ),
+                    tileColor: Colors.lightBlueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                  // category
-                  subtitle: Text(
-                    _value.category.name,
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        fontFamily: 'RaleWay'),
-                  ),
-                  tileColor: Colors.lightBlueAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
                 ),
-              ),
-            );
-          },
-          separatorBuilder: (ctx, index) {
-            return const SizedBox(
-              height: 10,
-            );
-          },
-          itemCount: newList.length,
-        );
-      },
+              );
+            },
+            separatorBuilder: (ctx, index) {
+              return const SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: newList.length,
+          );
+        },
+      ),
     );
   }
 
